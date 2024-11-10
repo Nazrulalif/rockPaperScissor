46bbe0ed16d544f591a9e6568c651bf1a90f90af
Nazrulalif <pumaddidaszero@gmail.com>
2024-11-10 18:09:34 +0800

    change phone screen responsive

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
 const MainLayout = () => {
   return (
     <>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen h-full flex flex-col overflow-hidden">
         <Navbar />
         <Outlet />
         <Footer />
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     <>
       <nav className="p-1 lg:p-3 py-5 lg:py-7 sticky top-0 z-1 shadow-sm bg-white">
         <a href="/">
-          <div className="ml-3 lg:ml-36 flex items-center opacity-70">
+          <div className="ml-3 lg:ml-36 flex items-center opacity-70 justify-center lg:justify-start md:justify-start">
             <i className="fas fa-hand-scissors mr-2 lg:mr-4 text-3xl lg:text-3xl"></i>
-            <span className="font-mono font-bold text-sm lg:text-xl tracking-wider">
+            <span className="font-mono font-bold text-sm lg:text-xl tracking-wider text-center lg:text-left">
               RockPaperScissor
             </span>
           </div>
